61db339ca762cc2b4d41d9ace8cefef582437b4d
Rodion Fedechkin <fedechkin@live.ru>
2022-07-16 13:12:24 +0300

    Commit

--- EFCore_Activity0302/Program.cs
+++ EFCore_Activity0302/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EFCore_DBLibrary;
+using InventoryModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -12,6 +13,8 @@ namespace EFCore_Activity0302
         static void Main(string[] args)
         {
             BuildOptions();
+            EnsureItems();
+            ListInventory();
         }
 
         static void BuildOptions()
@@ -19,6 +22,42 @@ namespace EFCore_Activity0302
             _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
             _optionBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
             _optionBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+        }
+
+        static void EnsureItems()
+        {
+            EnsureItem("Batman begins");
+            EnsureItem("Inception");
+            EnsureItem("Remember the Titans");
+            EnsureItem("Star Wars: The Empire strikes back");
+            EnsureItem("Top Gun");
+
+        }
+
+        static void EnsureItem(string name)
+        {
+            using (var db = new InventoryDbContext(_optionBuilder.Options))
+            {
+                var existingItem = db.Items.FirstOrDefault(item => item.Name.ToLower() == name.ToLower());
+                if (existingItem == null)
+                {
+                    var item = new Item
+                    {
+                        Name = name
+                    };
+                    db.Items.Add(item);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        static void ListInventory()
+        {
+            using (var db = new InventoryDbContext(_optionBuilder.Options))
+            {
+                var items = db.Items.OrderBy(item => item.Name).ToList();
+                items.ForEach(item => Console.WriteLine(item.Name));
+            }
         }
     }
 }
